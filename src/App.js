3e8000b5acbe1af3d71c44259acc89d5b74cdf10
Sylwia <sylwia_24_24@wp.pl>
2020-02-09 12:24:48 +0100

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -13,41 +13,55 @@ import Order from './components/views/Order/Order';
 import Ordering from './components/views/Ordering/Ordering';
 import NewOrder from './components/views/NewOrder/NewOrder';
 import NewBooking from './components/views/NewBooking/NewBooking';
+import { StylesProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#284C6F' }, 
+    //secondary: { main: '#11cb5f'}, 
+  },
+});
 
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomePage} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering} />
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={Order} />
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={NewOrder} />      
-          <Route exact path={`${process.env.PUBLIC_URL}/booking`} component={Booking} />
-          <Route exact path={`${process.env.PUBLIC_URL}/booking/new`} component={NewBooking} />
-          <Route exact path={`${process.env.PUBLIC_URL}/booking/:id`} component={BookingId} />
-          <div className="App">
-            <header className="App-header">
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-              Learn React
-              </a>
-            </header>
-          </div>
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomePage} />
+              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={Order} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={NewOrder} />      
+              <Route exact path={`${process.env.PUBLIC_URL}/booking`} component={Booking} />
+              <Route exact path={`${process.env.PUBLIC_URL}/booking/new`} component={NewBooking} />
+              <Route exact path={`${process.env.PUBLIC_URL}/booking/:id`} component={BookingId} />
+              <div className="App">
+                <header className="App-header">
+                  <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                Learn React
+                  </a>
+                </header>
+              </div>
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
   
